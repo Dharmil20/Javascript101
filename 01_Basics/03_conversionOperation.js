a5a3402a9fdae09830cad7af5c2acc806da13a31
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-11-20 17:22:19 +0000

    Add coversionOperations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+// console.log(typeof(isLoggedIn));
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+
+/* 
+1 => true; 0 => false
+"" => false
+"Dharmil" => true
+*/
+
+// **************************** Operations **************************** 
+let value = 3
+let negVaue = -value
+console.log(negVaue);
